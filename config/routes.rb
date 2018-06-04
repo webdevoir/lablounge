0c87fa61d5c6ea104d9b87a1fccc41b0ffcfabcc
Katherine Nguyen <khnguyen86@live.com>
2018-06-03 22:11:56 -0700

    adds routes for new about

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: { sessions: 'admin/sessions' }
 
   get '/admin', to: 'admin#dashboard'
+  get '/admin/about/:id', to: 'admin#about_edit', as: :about_edit
+  put '/admin/about/:id/update', to: 'admin#about_update', as: :about_update
+
   root 'home#homepage'
 end
