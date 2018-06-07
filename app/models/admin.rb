bf376f0dc5edfa1cd8d29535e9186c6d76f6a502
Katherine Nguyen <khnguyen86@live.com>
2018-06-06 19:26:59 -0700

    removes files

--- app/models/admin.rb
+++
@@ -1,8 +0,0 @@
-class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
-
-  validates :username, uniqueness: true
-end
