ee10b4a45591fd89576eec3b3d5e6535ec4dc76d
Katherine Nguyen <khnguyen86@live.com>
2018-06-13 22:49:08 -0700

    adds admin change email and pw functionality

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,6 +3,24 @@ module Admin
     before_action :authenticate_user!
 
     def dashboard
+      @user = current_user
+    end
+
+    def update_admin_account
+      @user = current_user
+      if @user.update(user_params)
+        flash[:notice] = "Account updated successfully."
+        redirect_to admin_dashboard_path
+      else
+        flash[:alert] = @user.errors.full_messages
+        redirect_to admin_dashboard_path
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
   validates :username, uniqueness: true
 end
