b4a900546488b11878349f2c1fd9be32c43621d7
Katherine Nguyen <khnguyen86@live.com>
2018-07-11 21:56:36 -0700

    updates navbar and scrolling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,43 +7,43 @@ $(document).ready(function() {
   // ========================================================================= //
 
 
-  $(document).on("scroll", onScroll);
+  // $(document).on("scroll", onScroll);
 
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+  // $('a[href^="#"]').on('click', function(e) {
+  //   e.preventDefault();
+  //   $(document).off("scroll");
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
-    });
+  //   $('a').each(function() {
+  //     $(this).removeClass('active');
+  //     if ($(window).width() < 768) {
+  //       $('.nav-menu').slideUp();
+  //     }
+  //   });
 
-    $(this).addClass('active');
+  //   $(this).addClass('active');
 
-    var target = this.hash,
-        menu = target;
+  //   var target = this.hash,
+  //       menu = target;
 
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
+  //   target = $(target);
+  //   $('html, body').stop().animate({
+  //     'scrollTop': target.offset().top - 80
+  //   }, 500, 'swing', function() {
+  //     window.location.hash = target.selector;
+  //     $(document).on("scroll", onScroll);
+  //   });
+  // });
 
 
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
+  // function onScroll(event) {
+  //   if ($('.home').length) {
+  //     var scrollPos = $(document).scrollTop();
+  //     $('nav ul li a').each(function() {
+  //       var currLink = $(this);
+  //       var refElement = $(currLink.attr("href"));
+  //     });
+  //   }
+  // }
 
   // ========================================================================= //
   //  //NAVBAR SHOW - HIDE
