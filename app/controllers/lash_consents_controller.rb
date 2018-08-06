c4753c27c1d4797df4b8be556b78139e78e2f30a
Katherine Nguyen <khnguyen86@live.com>
2018-08-05 17:02:49 -0700

    adds lash consent model and controller

--- app/controllers/lash_consents_controller.rb
+++ app/controllers/lash_consents_controller.rb
@@ -1,8 +1,46 @@
 class LashConsentsController < ApplicationController
+  #include Wicked::Wizard
+
+  #steps :client, :contact, :questionnaire
+
+  # def show
+  #   case step
+  #   when :client
+  #     @lash_consent = LashConsent.new(status: step.to_s)
+  #   when :contact, :questionnaire
+  #     @lash_consent = 
+  #   render_wizard
+  # end
+
   def new
     @lash_consent = LashConsent.new
   end
 
   def create
+    @lash_consent = LashConsent.new(lash_consent_params)
+    if @lash_consent.save
+      puts "success!"
+      flash[:notice] = "Successfully created an about section."
+      redirect_to root_path
+    else
+      flash[:alert] = @lash_consent.errors.full_messages
+      puts @lash_consent.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def lash_consent_params
+    params.require(:lash_consent).permit(
+      :first_name, :last_name, :email, :address1, :address2,
+      :city, :state, :zip, :phone, :lash_lift_before,
+      :allergies, :brows_lashes_tinted, :hair_color_allergy,
+      :birthdate, :status, {natural_lashes: []}, {medical_history: []},
+      :consent
+    )
   end
 end
--- app/models/lash_consent.rb
+++ app/models/lash_consent.rb
@@ -1,28 +1,57 @@
 class LashConsent < ApplicationRecord
-  validates :first_name, :last_name, :email, :phone, :birthdate,
-    :lash_lift_before, :natural_lashes, :brows_lashes_tinted, :hair_color_allergy, presence: true
+
+  validates :first_name, :last_name, :birthdate, presence: true
+  validates :email, :phone, presence: true
+  validates :natural_lashes, presence: true
+  validates :lash_lift_before, :brows_lashes_tinted, :hair_color_allergy, inclusion: {in: [true, false]}
+  validates :consent, inclusion: {in: [true]}
+
+  before_create :add_consent_timestamp
+
+  def add_consent_timestamp
+    self.consent_timestamp = Time.now
+  end
 
     NATURAL_LASHES = [
-      'short',
-      'long',
-      'thick',
-      'straight',
-      'frail'
+      'Short',
+      'Long',
+      'Thick',
+      'Straight',
+      'Frail'
     ].freeze
 
-    MEDICAL_PAST_ITEMS = [
-      'accutane/retin-a',
-      'allergies/watery eyes',
-      'allergies to latex',
-      'allergies to preservatives in saline solutions',
-      'allergies to acrylic nails',
-      'allergies to adhesive tape',
-      'alopecia',
-      'blepharitis',
-      'blepharoplasty',
-      'breast feeding',
-      'cataract',
-      'cataract surgey'
+    MEDICAL_HISTORY_ITEMS = [
+      'Accutane/Retin-a',
+      'Allergies/Watery eyes',
+      'Allergies to latex',
+      'Allergies to preservatives in saline solutions',
+      'Allergies to acrylic nails',
+      'Allergies to adhesive tape',
+      'Alopecia',
+      'Blepharitis',
+      'Blepharoplasty',
+      'Breast feeding',
+      'Cataract',
+      'Cataract surgey',
+      'Claustriphobia',
+      'Conjunctivitis contact lenses',
+      'Chemotherapy/radiation within the past 6 months',
+      'Compulsive eyelash pulling',
+      'Dry eye syndrome/sjorges syndrome',
+      'Diabetic retinopathy',
+      'Eczema/psorasis',
+      'Estremely oily skin',
+      'Glaucoma',
+      'Irritated/broken skin',
+      'Lash loss',
+      'Lasik eye surgery',
+      'Permanent make up',
+      'Pink eye/sty',
+      'Pregnant',
+      'Recent medical treatment to the eye, lids, or duct',
+      'Recent chemical peels',
+      'Sensitive skin',
+      'Thyroid medications'
     ].freeze
 
 end
