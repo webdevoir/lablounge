f2a3d8a3b3c72e0a87dfe5a691f0ffe816edaf14
Katherine Nguyen <khnguyen86@live.com>
2018-08-05 21:26:52 -0700

    configures letteropener mail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # LetterOpener
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.asset_host = "http://localhost:3000"
 end
